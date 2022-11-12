14e3c7d0651e727ed135dac66c7774dc8b99d07f
davique0 <davique@gmail.com>
2022-11-11 19:03:52 -0500

    lightcoin refactore, before getter and setter

--- index.js
+++ index.js
@@ -1,29 +1,52 @@
-let balance = 500.00;
+//let balance = 500.00;
 
-class Withdrawal {
+class Account {
 
-  constructor(amount) {
-    this.amount = amount;
+  constructor(username) {
+    this.username = username;
+    //starts every account at $0.00
+    this.balance = 0;
   }
+}
+
+class Transaction {
+  constructor(amount, account) {
+    this.amount = amount;
+    this.account = account;
+  }
+}
+
+class Withdrawal extends Transaction {
 
   commit() {
-    balance -= this.amount;
+    this.account.balance -= this.amount;
   }
 
 }
 
+class Deposit extends Transaction {
 
 
+  commit() {
+    this.account.balance += this.amount;
+  }
+}
 
 // DRIVER CODE BELOW
 // We use the code below to "drive" the application logic above and make sure it's working as expected
 
-t1 = new Withdrawal(50.25);
+const myAccount = new Account("snow-patrol");
+
+t1 = new Withdrawal(50.25, myAccount);
 t1.commit();
 console.log('Transaction 1:', t1);
 
-t2 = new Withdrawal(9.99);
+t2 = new Withdrawal(9.99, myAccount);
 t2.commit();
 console.log('Transaction 2:', t2);
 
-console.log('Balance:', balance);
+t3 = new Deposit(120.00, myAccount);
+t3.commit();
+console.log('Transaction 3:', t3);
+
+console.log('Balance:', myAccount.balance);
